95c5bd051557fefdf9d7f723cd5675da0600be13
Kauan Gonçalves <73912483+kaugoncalves@users.noreply.github.com>
2021-01-26 17:58:30 -0300

    aula 2 update

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
-import styled from 'styled-components'
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
 import db from '../db.json';
-import Widget from '../src/components/Widget/Index.js'
-import QuizLogo from '../src/components/QuizLogo/Index.js'
-import QuizBackground from '../src/components/QuizBackground/Index.js' 
-import Footer from '../src/components/Footer/Index.js'
-import GitHubCorner from '../src/components/GithubCorner/Index.js'
-
+import Widget from '../src/components/Widget/Index.js';
+import QuizLogo from '../src/components/QuizLogo/Index.js';
+import QuizBackground from '../src/components/QuizBackground/Index.js';
+import Footer from '../src/components/Footer/Index.js';
+import GitHubCorner from '../src/components/GithubCorner/Index.js';
+import { useRouter } from 'next/router';
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -16,41 +20,59 @@ export const QuizContainer = styled.div`
     margin: auto;
     padding: 15px;
   }
-`
-
-
-
-
+`;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
+
   return (
 
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>Alura quiz - Pokemon</title>
+      </Head>
       <QuizContainer>
-
+        <QuizLogo />
 
         <Widget>
-        <Widget.Header>
-              Pokemon
-      </Widget.Header>
+          <Widget.Header>
+            Pokemon
+          </Widget.Header>
 
           <Widget.Content>
-            <h1>Salve salve</h1>
+            <p> Teste aqui seues conhecimentos em jogos de pokemon! </p>
+            <form onSubmit={function (infosEvento) {
+              infosEvento.preventDefault();
+              router.push(`/quiz?name=${name}`)
+              console.log("Fez um submit");
+            }}> 
 
-            <p>aaaaaaaaaaaaaaa</p>
+
+            <input onChange={function (infosEvento) {
+              setName(infosEvento.target.value);
+              console.log(infosEvento.target.value);
+
+            }} 
+            
+            placeholder="Fale seu nome" />
+            <button type="submit" disabled={name.length === 0}>
+              jogar {name}
+            </button>
+            </form>
           </Widget.Content>
         </Widget>
 
-
         <Widget>
-        <Widget.Header>
-              Pokemon
-      </Widget.Header>
-      
-          <Widget.Content>
-            <h1>Salve salve2</h1>
+          <Widget.Header>
+            Quizes da galera
+          </Widget.Header>
 
-            <p>aaaaaaaaaaaaaaa2</p>
+          <Widget.Content>
+            <h1>Oia la hein</h1>
+
+            <p>aqui</p>
           </Widget.Content>
         </Widget>
         <Footer />
